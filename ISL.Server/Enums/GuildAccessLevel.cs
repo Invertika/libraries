f73efba3c6fe43f33865ddfad72e724d697d86d5
seeseekey <seeseekey@googlemail.com>
2012-04-05 16:58:39 +0200

    handleGuildKickMember implementiert.

---
+++ ISL.Server/Enums/GuildAccessLevel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISL.Server.Enums
+{
+	/**
+ * Guild member permissions
+ * Members with NONE cannot invite users or set permissions
+ * Members with TOPIC_CHANGE can change the guild channel topic
+ * Members with INVITE can invite other users
+ * Memeber with KICK can remove other users
+ * Members with OWNER can invite users and set permissions
+ */
+	public enum GuildAccessLevel
+	{
+		GAL_NONE=0,
+		GAL_TOPIC_CHANGE=1,
+		GAL_INVITE=2,
+		GAL_KICK=4,
+		GAL_OWNER=255
+	}
+}
