a23b35bc643db31470d0b571cc0d5907b018917c
seeseekey <seeseekey@gmail.com>
2012-08-18 19:50:25 +0200

    InventoryItem und Possession implementiert.

---
+++ ISL.Server/Common/InventoryItem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ISL.Server
+{
+    public class InventoryItem
+    {
+        public InventoryItem()
+        {
+        }
+    }
+}
+
---
+++ ISL.Server/Common/Possessions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ISL.Server
+{
+    public class Possessions_
+    {
+        public Possessions_()
+        {
+        }
+    }
+}
+
