b405485b9d313900e8072e919e53bdb00f1816ae
seeseekey <seeseekey@gmail.com>
2012-08-20 16:24:03 +0200

    Chrakterliste wird nun initialisiert.

--- ISL.Server/Account/Account.cs
+++ ISL.Server/Account/Account.cs
@@ -32,210 +32,211 @@ using System.Diagnostics;
 
 namespace ISL.Server.Account
 {
-    public class Account
-    {
-        //        Account(const Account &rhs);
-        //Account &operator=(const Account &rhs);
+	public class Account
+	{
+		//        Account(const Account &rhs);
+		//Account &operator=(const Account &rhs);
 
-        string mName;        /**< User name */
-        string mPassword;    /**< User password (hashed with salt) */
-        string mRandomSalt;  /**< A random sequence sent to client to
+		string mName;        /**< User name */
+		string mPassword;    /**< User password (hashed with salt) */
+		string mRandomSalt;  /**< A random sequence sent to client to
                                        protect against replay attacks.*/
-        string mEmail;       /**< User email address (hashed) */
-        Dictionary<uint, Character> mCharacters;   /**< Character data */
-        int mID;                  /**< Unique id */
-        byte mLevel;     /**< Account level */
-        DateTime mRegistrationDate; /**< Date and time of the account registration */
-        DateTime mLastLogin;        /**< Date and time of the last login */
+		string mEmail;       /**< User email address (hashed) */
+		Dictionary<uint, Character> mCharacters;   /**< Character data */
+		int mID;                  /**< Unique id */
+		byte mLevel;     /**< Account level */
+		DateTime mRegistrationDate; /**< Date and time of the account registration */
+		DateTime mLastLogin;        /**< Date and time of the last login */
 
-        public Account(int id = -1)
-        {
-            mID = id;
-        }
+		public Account(int id=-1)
+		{
+			mID=id;
+			mCharacters=new Dictionary<uint, Character>();
+		}
 
-        /// <summary>
-        /// Get all the characters.
-        /// </summary>
-        /// <returns>all the characters.</returns>
-        public Dictionary<uint, Character> getCharacters()
-        {
-            return mCharacters;
-        }
+		/// <summary>
+		/// Get all the characters.
+		/// </summary>
+		/// <returns>all the characters.</returns>
+		public Dictionary<uint, Character> getCharacters()
+		{
+			return mCharacters;
+		}
 
-        bool isSlotEmpty(uint slot)
-        {
-            return !mCharacters.ContainsKey(slot);
-        }
+		bool isSlotEmpty(uint slot)
+		{
+			return !mCharacters.ContainsKey(slot);
+		}
 
-        public void setCharacters(Dictionary<uint, Character> characters)
-        {
-            mCharacters = characters;
-        }
+		public void setCharacters(Dictionary<uint, Character> characters)
+		{
+			mCharacters=characters;
+		}
 
-        void addCharacter(Character character)
-        {
-            uint slot = character.getCharacterSlot();
-            mCharacters [slot] = character;
-        }
+		void addCharacter(Character character)
+		{
+			uint slot=character.getCharacterSlot();
+			mCharacters[slot]=character;
+		}
 
-        void delCharacter(uint slot)
-        {
-            mCharacters.Remove(slot);
-        }
+		void delCharacter(uint slot)
+		{
+			mCharacters.Remove(slot);
+		}
 
-        void setID(int id)
-        {
-            Debug.Assert(mID < 0);
-            mID = id;
-        }
+		void setID(int id)
+		{
+			Debug.Assert(mID<0);
+			mID=id;
+		}
 
-        //void setRegistrationDate(time_ ttime)
-        //{
-        //    //mRegistrationDate = time;
-        //}
+		//void setRegistrationDate(time_ ttime)
+		//{
+		//    //mRegistrationDate = time;
+		//}
 
-        public void setLastLogin(DateTime time)
-        {
-            mLastLogin = time;
-        }
+		public void setLastLogin(DateTime time)
+		{
+			mLastLogin=time;
+		}
 
-        /**
+		/**
 		 * Get the account level.
 		 *
 		 * @return the account level.
 		 */
-        public int getLevel()
-        {
-            return mLevel;
-        }
+		public int getLevel()
+		{
+			return mLevel;
+		}
 
-        /**
+		/**
    * Get account ID.
    *
    * @return the unique ID of the account, a negative number if none yet.
    */
-        public int getID()
-        {
-            return mID;
-        }
+		public int getID()
+		{
+			return mID;
+		}
 
-        /**
-         * Get the user name.
-         *
-         * @return the user name.
-         */
-        public string getName()
-        {
-            return mName;
-        }
+		/**
+		 * Get the user name.
+		 *
+		 * @return the user name.
+		 */
+		public string getName()
+		{
+			return mName;
+		}
 
-        /**
-         * Get the user password (hashed with salt).
-         *
-         * @return the user password (hashed with salt).
-         */
-        public string getPassword()
-        {
-            return mPassword;
-        }
+		/**
+		 * Get the user password (hashed with salt).
+		 *
+		 * @return the user password (hashed with salt).
+		 */
+		public string getPassword()
+		{
+			return mPassword;
+		}
 
-        /**
-         * Get the user random salt.
-         *
-         * @return the random salt used for next login.
-         */
-        public string getRandomSalt()
-        {
-            return mRandomSalt;
-        }
+		/**
+		 * Get the user random salt.
+		 *
+		 * @return the random salt used for next login.
+		 */
+		public string getRandomSalt()
+		{
+			return mRandomSalt;
+		}
 
-        /**
-         * Get the time of the last login.
-         */
-        public DateTime getLastLogin()
-        {
-            return mLastLogin;
-        }
+		/**
+		 * Get the time of the last login.
+		 */
+		public DateTime getLastLogin()
+		{
+			return mLastLogin;
+		}
 
-        /**
-         * Set the user name.
-         *
-         * @param name the user name.
-         */
-        public void setName(string name)
-        {
-            mName = name;
-        }
+		/**
+		 * Set the user name.
+		 *
+		 * @param name the user name.
+		 */
+		public void setName(string name)
+		{
+			mName=name;
+		}
 
-        /**
-         * Set the user password. The password is expected to be already
-         * hashed with a salt.
-         *
-         * The hashing must be performed externally from this class or else
-         * we would end up with the password being hashed many times
-         * (e.g setPassword(getPassword()) would hash the password twice.
-         *
-         * @param password the user password (hashed with salt).
-         */
-        public void setPassword(string password)
-        {
-            mPassword = password;
-        }
+		/**
+		 * Set the user password. The password is expected to be already
+		 * hashed with a salt.
+		 *
+		 * The hashing must be performed externally from this class or else
+		 * we would end up with the password being hashed many times
+		 * (e.g setPassword(getPassword()) would hash the password twice.
+		 *
+		 * @param password the user password (hashed with salt).
+		 */
+		public void setPassword(string password)
+		{
+			mPassword=password;
+		}
 
-        /**
-         * Set the user email address. The email address is expected to be
-         * already hashed.
-         *
-         * @param email the user email address (hashed).
-         */
-        public void setEmail(string email)
-        {
-            mEmail = email;
-        }
+		/**
+		 * Set the user email address. The email address is expected to be
+		 * already hashed.
+		 *
+		 * @param email the user email address (hashed).
+		 */
+		public void setEmail(string email)
+		{
+			mEmail=email;
+		}
 
-        /**
-         * Set the account level.
-         *
-         * @param level the new level.
-         */
-        public void setLevel(int level)
-        {
-            mLevel = (byte)level;
-        }
+		/**
+		 * Set the account level.
+		 *
+		 * @param level the new level.
+		 */
+		public void setLevel(int level)
+		{
+			mLevel=(byte)level;
+		}
 
-        public void setRegistrationDate(DateTime time)
-        {
-            mRegistrationDate = time;
-        }
+		public void setRegistrationDate(DateTime time)
+		{
+			mRegistrationDate=time;
+		}
 
-        /**
-         * Get the user email address (hashed).
-         *
-         * @return the user email address (hashed).
-         */
-        public string getEmail()
-        {
-            return mEmail;
-        }
+		/**
+		 * Get the user email address (hashed).
+		 *
+		 * @return the user email address (hashed).
+		 */
+		public string getEmail()
+		{
+			return mEmail;
+		}
 
-        /**
-         * Get the time of the account registration.
-         */
-        public DateTime getRegistrationDate()
-        {
-            return mRegistrationDate;
-        }
+		/**
+		 * Get the time of the account registration.
+		 */
+		public DateTime getRegistrationDate()
+		{
+			return mRegistrationDate;
+		}
 
-        /**
-         * Set the random salt. This salt is sent to the client, so the client
-         * can hash its password with this random salt.
-         * This will help to protect against replay attacks.
-         *
-         * @param the new random salt to be sent out next login
-         */
-        public void setRandomSalt(string salt)
-        {
-            mRandomSalt = salt;
-        }
-    }
+		/**
+		 * Set the random salt. This salt is sent to the client, so the client
+		 * can hash its password with this random salt.
+		 * This will help to protect against replay attacks.
+		 *
+		 * @param the new random salt to be sent out next login
+		 */
+		public void setRandomSalt(string salt)
+		{
+			mRandomSalt=salt;
+		}
+	}
 }
