2a7d5e1b0b3963dfc1d824fae0340eeadbce5de5
seeseekey <seeseekey@gmail.com>
2012-12-22 19:44:49 +0100

    writeString Methode auf Serverseite implementiert.

--- ISL.Server/Network/MessageOut.cs
+++ ISL.Server/Network/MessageOut.cs
@@ -33,61 +33,65 @@ using ISL.Server.Common;
 
 namespace ISL.Server.Network
 {
-	public class MessageOut
-	{
-		MemoryStream data;
-		BinaryWriter writer;
+    public class MessageOut
+    {
+        MemoryStream data;
+        BinaryWriter writer;
 
-		public MessageOut()
-		{
-			data=new MemoryStream();
-			writer=new BinaryWriter(data);
-		}
+        public MessageOut()
+        {
+            data=new MemoryStream();
+            writer=new BinaryWriter(data);
+        }
 
-		public MessageOut(Protocol id)
-		{
-			data=new MemoryStream();
-			writer=new BinaryWriter(data);
+        public MessageOut(Protocol id)
+        {
+            data=new MemoryStream();
+            writer=new BinaryWriter(data);
 
-			writer.Write((UInt16)id);
-		}
+            writer.Write((UInt16)id);
+        }
 
-		public void writeInt8(int value)
-		{
-			writer.Write((sbyte)value);
-		}
+        public void writeInt8(int value)
+        {
+            writer.Write((sbyte)value);
+        }
 
-		public void writeInt16(int value)
-		{
-			writer.Write((Int16)value);
-		}
+        public void writeInt16(int value)
+        {
+            writer.Write((Int16)value);
+        }
 
-		public void writeInt32(int value)
-		{
-			writer.Write((Int32)value);
-		}
+        public void writeInt32(int value)
+        {
+            writer.Write((Int32)value);
+        }
 
-		public void writeDouble(double value)
-		{
-			writer.Write(value);
-		}
+        public void writeDouble(double value)
+        {
+            writer.Write(value);
+        }
 
-		public void writeString(string value)
-		{
-			writer.Write(value);
-		}
+        public void writeString(string value)
+        {
+            UTF8Encoding encoding=new UTF8Encoding();
+            byte[] bytesOfString=encoding.GetBytes(value);
 
-		 public byte[] getData() 
-		 { 
-			 return data.ToArray(); 
-		 }
+            writer.Write((Int16)bytesOfString.Length);
+            writer.Write(bytesOfString);        
+        }
+
+        public byte[] getData()
+        { 
+            return data.ToArray(); 
+        }
 
         /**
          * Returns the length of the data.
          */
-		public uint getLength()  
-		{ 
-			return (uint)data.Length; 
-		}
-	}
+        public uint getLength()
+        { 
+            return (uint)data.Length; 
+        }
+    }
 }
