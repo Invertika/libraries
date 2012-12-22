cea008caa0f33d76444478965e2855fa5ba7877c
seeseekey <seeseekey@gmail.com>
2012-12-22 19:32:21 +0100

    readString Methode für binäre Daten korrekt implementiert.

--- ISL.Server/Network/MessageIn.cs
+++ ISL.Server/Network/MessageIn.cs
@@ -33,76 +33,82 @@ using ISL.Server.Common;
 
 namespace ISL.Server.Network
 {
-	public class MessageIn
-	{
-		BinaryReader reader;
+    public class MessageIn
+    {
+        BinaryReader reader;
 
-		byte[] mData;            /**< Packet data */
-		ushort mLength;       /**< Length of data in bytes */
-		Protocol mId;           /**< The message ID. */
+        byte[] mData;            /**< Packet data */
+        ushort mLength;       /**< Length of data in bytes */
+        Protocol mId;           /**< The message ID. */
 
-		/**
+        /**
 		 * Actual position in the packet. From 0 to packet.length. A value
 		 * bigger than packet.length means EOP was reached when reading it.
 		 */
-		ushort mPos;
+        ushort mPos;
 
-		//TODO Länge und Position noch benötigt?
-		//TODO Typenkonvertierung? ushort etc.
+        //TODO Länge und Position noch benötigt?
+        //TODO Typenkonvertierung? ushort etc.
 
-		public MessageIn(byte[] data)
-		{
-			mData=data;
-			mLength=(ushort)data.Length;
-			mPos=0;
+        public MessageIn(byte[] data)
+        {
+            mData=data;
+            mLength=(ushort)data.Length;
+            mPos=0;
 
-			reader=new BinaryReader(new MemoryStream(data));
+            reader=new BinaryReader(new MemoryStream(data));
 			
-			// Read the message ID
-			mId=(Protocol)readInt16();
-		}
+            // Read the message ID
+            mId=(Protocol)readInt16();
+        }
 
-		/**
+        /**
  * Returns the message ID.
  */
-		public Protocol getId()
-		{
-			return mId;
-		}
+        public Protocol getId()
+        {
+            return mId;
+        }
 
-		public byte readInt8()
-		{
-			return reader.ReadByte();
-		}
+        public byte readInt8()
+        {
+            return reader.ReadByte();
+        }
 
-		public short readInt16()
-		{
-			return reader.ReadInt16();
-		}
+        public short readInt16()
+        {
+            return reader.ReadInt16();
+        }
 
-		public int readInt32()
-		{
-			return reader.ReadInt32();
-		}
+        public int readInt32()
+        {
+            return reader.ReadInt32();
+        }
 
-		public double readDouble()
-		{
-			return reader.ReadDouble();
-		}
+        public double readDouble()
+        {
+            return reader.ReadDouble();
+        }
 
-		public string readString()
-		{
-			return reader.ReadString();
-		}
+        public string readString()
+        {
+            short lengthOfString=readInt16();
+            byte[] bytesOfString=reader.ReadBytes(lengthOfString);
 
-		public override string ToString()
-		{
-			return mId.ToString();
-		}
+            UTF8Encoding encoding=new UTF8Encoding();
+            return encoding.GetString(bytesOfString);
 
-		public int getUnreadLength() 
-		{
-			return (int)(reader.BaseStream.Length-reader.BaseStream.Position);
-		}
-	}
+            //return reader.ReadString();
+        }
+
+        public override string ToString()
+        {
+            return mId.ToString();
+        }
+
+        public int getUnreadLength()
+        {
+            return (int)(reader.BaseStream.Length-reader.BaseStream.Position);
+        }
+    }
 }
