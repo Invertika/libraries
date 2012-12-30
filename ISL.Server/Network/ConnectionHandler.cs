60a75285c94ae20f3e4073d8039dcecd44d8f27a
seeseekey <seeseekey@gmail.com>
2012-12-30 21:27:33 +0100

    Unnötigen Quelltest entfernt.

--- ISL.Server/Network/ConnectionHandler.cs
+++ ISL.Server/Network/ConnectionHandler.cs
@@ -185,65 +185,6 @@ namespace ISL.Server.Network
                 // Stop listening for new clients.
                 listener.Stop();
             }
-
-            //ENetEvent event;
-            //// Process Enet events and do not block.
-            //while (enet_host_service(host, &event, timeout) > 0) {
-            //    switch (event.type) {
-            ////////////        case ENET_EVENT_TYPE_CONNECT:
-            ////////////        {
-            ////////////            NetComputer *comp = computerConnected(event.peer);
-            ////////////            clients.push_back(comp);
-            ////////////            LOG_INFO("A new client connected from " << *comp << ":"
-            ////////////                     << event.peer.address.port << " to port "
-            ////////////                     << host.address.port);
-
-            ////////////            // Store any relevant client information here.
-            ////////////            event.peer.data = (void *)comp;
-            ////////////        } break;
-
-            //        case ENET_EVENT_TYPE_RECEIVE:
-            //        {
-            //            NetComputer *comp =
-            //                static_cast<NetComputer*>(event.peer.data);
-
-            //            // If the scripting subsystem didn't hook the message
-            //            // it will be handled by the default message handler.
-
-            //            // Make sure that the packet is big enough (> short)
-            //            if (event.packet.dataLength >= 2) {
-            //                MessageIn msg((char *)event.packet.data,
-            //                              event.packet.dataLength);
-            //                LOG_DEBUG("Received message " << msg << " from "
-            //                          << *comp);
-
-            //                gBandwidth.increaseClientInput(comp, event.packet.dataLength);
-
-            //                processMessage(comp, msg);
-            //            } else {
-            //                LOG_ERROR("Message too short from " << *comp);
-            //            }
-
-            //            /* Clean up the packet now that we're done using it. */
-            //            enet_packet_destroy(event.packet);
-            //        } break;
-
-            //        case ENET_EVENT_TYPE_DISCONNECT:
-            //        {
-            //            NetComputer *comp =
-            //                static_cast<NetComputer*>(event.peer.data);
-
-            //            LOG_INFO("" << *comp << " disconnected.");
-
-            //            // Reset the peer's client information.
-            //            computerDisconnected(comp);
-            //            clients.erase(std::find(clients.begin(), clients.end(), comp));
-            //            event.peer.data = NULL;
-            //        } break;
-
-            //        default: break;
-            //    }
-            //}
         }
 
         protected void sendToEveryone(MessageOut msg)
