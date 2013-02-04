223b673367ed0240fc6495d11cc4959cd5473bcd
seeseekey <seeseekey@gmail.com>
2013-02-04 22:55:39 +0100

    Rectangle weiter implementiert.

--- ISL.Server/Utilities/Rectangle.cs
+++ ISL.Server/Utilities/Rectangle.cs
@@ -31,30 +31,30 @@ using System.Text;
 
 namespace ISL.Server.Utilities
 {
-	/**
+    /**
 	 * A rectangle in positive space. Usually represents a pixel-based zone on a
 	 * map.
 	 */
-	public class Rectangle
-	{
-		//public:
-		//    int x; /**< x coordinate */
-		//    int y; /**< y coordinate */
-		//    int w; /**< width */
-		//    int h; /**< height */
+    public class Rectangle
+    {
+        //public:
+        public   int x; /**< x coordinate */
+        public   int y; /**< y coordinate */
+        public   int w; /**< width */
+        public   int h; /**< height */
 
-		//    bool contains(const Point &p) const
-		//    {
-		//        return (p.x >= x && p.x < x + w &&
-		//                p.y >= y && p.y < y + h);
-		//    }
+        //    bool contains(const Point &p) const
+        //    {
+        //        return (p.x >= x && p.x < x + w &&
+        //                p.y >= y && p.y < y + h);
+        //    }
 
-		//    bool intersects(const Rectangle &r) const
-		//    {
-		//        return x < (r.x + r.w) &&
-		//               y < (r.y + r.h) &&
-		//               x + w > r.x &&
-		//               y + h > r.y;
-		//    }
-	}
+        //    bool intersects(const Rectangle &r) const
+        //    {
+        //        return x < (r.x + r.w) &&
+        //               y < (r.y + r.h) &&
+        //               x + w > r.x &&
+        //               y + h > r.y;
+        //    }
+    }
 }
